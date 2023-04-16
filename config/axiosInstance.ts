8e37a7e6e02e4ef5997aaf8f3faf87c491cd68b6
Victor Onofiok <48057500+radoncreep@users.noreply.github.com>
2023-04-16 20:15:47 +0100

    Update axiosInstance.ts

--- config/axiosInstance.ts
+++ config/axiosInstance.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    // baseURL: "http://192.168.0.140:8080/",
-    baseURL: "http://172.20.10.12:8080/",
+    baseURL: "",
     headers: {
         "Content-Type": "application/json"
     },
